78e357642bb9d9ef2c8129240d451ff086a28b91
Zac Garland <zachary.garland@outlook.com>
2023-06-19 19:46:43 -0500

    changed to server side api

--- src/App.js
+++ src/App.js
@@ -73,6 +73,8 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 };
 
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
 /*
   const [searchTerm, setSearchTerm] = React.useState(
     localStorage.getItem("search") || "React"
@@ -158,20 +160,20 @@ const App = () => {
   });
 
   React.useEffect(() => {
+if(!searchTerm) return;
+
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    // setIsLoading(true);
-    getAsyncStories()
+
+    fetch(`${API_ENDPOINT}${searchTerm}`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
-
-        //        setStories(result.data.stories);
-        // setIsLoading(false);
-      }) // setIsError(true)
+      })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -208,7 +210,7 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading....</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </>
   );
